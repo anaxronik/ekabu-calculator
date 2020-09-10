a51b9eb216ef97d8664918326817c197cbca08d5
Sergey Komarov <anaxronik@gmail.com>
2020-09-10 17:16:42 +0500

    fix imports

--- src/pages/Pricing/AddMaterial.jsx
+++ src/pages/Pricing/AddMaterial.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { CalcGroup } from "../../components/CalcGroup";
 import { Row, Col, Form, Button } from "react-bootstrap";
 import firebase from "../../db/firebase";
 
--- src/pages/Pricing/AddMaterialCategory.jsx
+++ src/pages/Pricing/AddMaterialCategory.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { CalcGroup } from "../../components/CalcGroup";
 import { Form, Button } from "react-bootstrap";
 import firebase from "../../db/firebase";
 
